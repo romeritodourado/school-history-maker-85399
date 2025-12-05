95e5d40fa1c4f2d268f50052d3156799e79a92f4
[dyad] <romeritorms@hotmail.com>
2025-12-04 22:57:25 -0300

    [dyad] Corrigindo a ordem do AuthProvider e BrowserRouter - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext"; // <-- Aqui está a importação
-import ProtectedRoute from "./components/ProtectedRoute"; // Corrigido: Importando do diretório components
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import InitialSuperAdminSetup from "./pages/InitialSuperAdminSetup";
 import MunicipalNetworkSetup from "./pages/MunicipalNetworkSetup";
@@ -35,11 +35,11 @@ const queryClient = new QueryClient({
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <AuthProvider> {/* <-- Aqui ele é usado, envolvendo o BrowserRouter */}
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
+      <BrowserRouter> {/* BrowserRouter agora é o componente mais externo para rotas */}
+        <AuthProvider> {/* AuthProvider agora está dentro do BrowserRouter */}
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
             <Routes>
               <Route path="/login" element={<Login />} />
               <Route path="/initial-superadmin-setup" element={<InitialSuperAdminSetup />} />
@@ -113,8 +113,8 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
-        </TooltipProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </ThemeProvider>
   </QueryClientProvider>
 );
