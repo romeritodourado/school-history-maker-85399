e5b1e6a8eb30e2db6fecc6b1f7a658ab56ccee79
[dyad] <romeritorms@hotmail.com>
2025-12-06 14:43:13 -0300

    [dyad] Corrigindo erro de sintaxe JSX no Dialog de pré-visualização - wrote 1 file(s)

--- src/pages/SignTranscripts.tsx
+++ src/pages/SignTranscripts.tsx
@@ -785,7 +785,7 @@ export default function SignTranscripts() {
             <DialogDescription>
               Verifique os detalhes do histórico antes de assinar.
             </DialogDescription>
-          </CardHeader>
+          </DialogHeader> {/* Correct closing tag for DialogHeader */}
           {loadingPreview ? (
             <div className="flex items-center justify-center py-8">
               <Loader2 className="h-8 w-8 animate-spin text-primary" />
